8863fa75f0522895b2587b84f2ce682861798276
thuananh0611 <73033939+thuananh0611@users.noreply.github.com>
2020-10-24 19:09:16 +0700

    add entities

---
+++ project_1/project_1/Entities/K_Hang.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using project_1.Utility;
+using System.Text;
+
+namespace project_1.Entities
+{
+    //Khai báo các thông tin liên quan đến khách hàng
+    class K_Hang
+    {
+        #region Các thành phần dữ liệu
+            private int makh;
+            private string hoten;
+            private string diachi;
+            private DateTime ngaysinh;
+            private string sdt;
+        #endregion
+
+        #region Các thuộc tính
+        public int Makh
+        {
+            get { return makh; }
+            set
+            {
+                if (value >= 1)
+                    makh = value;
+            }
+        }
+        public string Hoten
+        {
+            get
+            {
+                return hoten;
+            }
+            set
+            {
+                if (value != "")
+                    hoten = value;
+            }
+        }
+        public string Diachi
+        {
+            get { return diachi; }
+            set
+            {
+                if (value != "")
+                    diachi = value;
+            }
+        }
+        public DateTime Ngaysinh
+        {
+            get { return ngaysinh; }
+            set
+            {
+                ngaysinh = value;
+            }
+        }
+        public string Sdt
+        {
+            get { return sdt; }
+            set { sdt = value; }
+        }
+        #endregion
+
+        #region Các phương thức
+        public K_Hang() { }
+        //Phương thức thiết lập sao chép
+        public K_Hang(K_Hang kh)
+        {
+            this.makh = kh.makh;
+            this.hoten = kh.hoten;
+            this.diachi = kh.diachi;
+            this.ngaysinh = kh.ngaysinh;
+            this.sdt = kh.sdt;
+        }
+        public K_Hang(int makh, string hoten, string diachi, DateTime ngaysinh, string sdt)
+        {
+            this.makh = makh;
+            this.hoten = hoten;
+            this.diachi = diachi;
+            this.ngaysinh = ngaysinh;
+            this.sdt = sdt;
+        }
+        #endregion
+    }
+}
---
+++ project_1/project_1/Utility/Menu.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project_1.Utility
+{
+    public abstract class Menu
+    {
+        private string[] mn;
+        public Menu(string[] mn)
+        {
+            this.mn = mn;
+        }
+        public int MaxMuc()
+        {
+            int max = mn[0].Length;
+            for (int i = 1; i < mn.Length; ++i)
+                if (max < mn[i].Length)
+                    max = mn[i].Length;
+            return max;
+        }
+        public void ChuanHoaMenu()
+        {
+            int max = MaxMuc();
+            for (int i = 0; i < mn.Length; ++i)
+                mn[i] = project_1.Utility.CongCu.ChuanHoaXau(mn[i], max);
+        }
+        public void Writexy(int x, int y, int vitri, ConsoleColor maunen, ConsoleColor mauchu)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.BackgroundColor = maunen;
+            Console.ForegroundColor = mauchu;
+            Console.Write(mn[vitri]);
+        }
+        public void Writexy(int x, int y, string s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
+        }
+        public void HienTheoPhimTat(int x, int y, ConsoleColor maunen, ConsoleColor mauchu)
+        {
+            ChuanHoaMenu();
+            IO.BoxTitle("CAC CHUC NANG CHINH", x, y, mn.Length * 2 + 5, MaxMuc() + 10);
+            x = x + 5;
+            y = y + 3;
+            for (int i = 0; i < mn.Length; ++i) Writexy(x, y + i * 2, i, maunen, mauchu);
+            IO.Writexy("Hay chon mot chuc nang de thuc hien...", x, y + mn.Length * 2);
+            ConsoleKeyInfo kt = Console.ReadKey();
+
+            string[] key = new string[mn.Length];
+            for (int i = 0; i < mn.Length; ++i)
+                key[i] = mn[i].Substring(0, mn[i].IndexOf("."));
+            for (int i = 0; i < key.Length; ++i)
+                if (kt.Key.ToString() == key[i]) ThucHien(i);
+        }
+        public void HienTheoKieuCuon(int x, int y, ConsoleColor maunen_t, ConsoleColor mauchu_t, ConsoleColor maunen_s, ConsoleColor mauchu_s)
+        {
+            ChuanHoaMenu();
+            int chon = 0;
+            for (int i = 0; i < mn.Length; ++i) Writexy(x, y + i, i, maunen_t, mauchu_t);
+            Writexy(x, y + chon, chon, maunen_s, mauchu_s);
+            do
+            {
+                ConsoleKeyInfo kt = Console.ReadKey();
+                for (int i = 0; i < mn.Length; ++i) Writexy(x, y + i, i, maunen_t, mauchu_t);
+                switch (kt.Key)
+                {
+                    case ConsoleKey.DownArrow://↓
+                        if (chon == mn.Length - 1) chon = 0;
+                        else chon = chon + 1;
+                        Writexy(x, y + chon, chon, maunen_s, mauchu_s);
+                        break;
+                    case ConsoleKey.UpArrow://↑
+                        if (chon == 0) chon = mn.Length - 1;
+                        else chon = chon - 1;
+                        Writexy(x, y + chon, chon, maunen_s, mauchu_s);
+                        break;
+                    case ConsoleKey.Enter:
+                        ThucHien(chon);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        for (int i = 0; i < mn.Length; ++i) Writexy(x, y + i, i, maunen_t, mauchu_t);
+                        Writexy(x, y + chon, chon, maunen_s, mauchu_s);
+                        break;
+                }
+                Writexy(x, y + mn.Length + 1, "Ban dang chon muc:" + chon);
+            } while (true);
+        }
+        public abstract void ThucHien(int vitri);
+    }
+}
